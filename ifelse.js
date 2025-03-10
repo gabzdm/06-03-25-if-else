e22cc0612acd0efe1038366f3f0c50c0eac72d65
gabzdm <gabrieladamasceno42@gmail.com>
2025-03-10 14:29:06 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+function verificar() {
+let txtLogin = document.getElementByld("txtLogin"); 
+let txtSenha = document.getElementById("txtSenha");
+
+let login = "teste";
+let senha = "teste";
+
+if (txtLogin.volue == login && txtSenha.volue == senha) {
+    console.log("logado com sucesso!")
+} else {
+    console.log("login ou senha incorretos! Digite e tente novamente.");
+    
+}
+}
